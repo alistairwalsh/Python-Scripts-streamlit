58fe8cbdbe0101564f6bb2f65cb50eb248b79830
alistairwalsh <alistairwalsh@me.com>
2021-11-02 09:49:26 +1100

    latex formula

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ st.title("NMF")
 
 st.image('nmf/1920px-Restricted_Boltzmann_machine.svg.png')
 
-st.write('NMF as a probabilistic graphical model: visible units (V) are connected to hidden units (H) through weights W, so that V is generated from a probability distribution with mean {\displaystyle \sum _{a}W_{ia}h_{a}}\sum _{a}W_{ia}h_{a}')
+st.write('NMF as a probabilistic graphical model: visible units (V) are connected to hidden units (H) through weights W, so that V is generated from a probability distribution with mean ')
+
+st.latex('{\displaystyle \sum _{a}W_{ia}h_{a}}\sum _{a}W_{ia}h_{a}')
