3e56f0410522ba4ffc3357d681e16b445573122a
alistairwalsh <alistairwalsh@me.com>
2021-11-05 11:23:17 +1100

    added clustering

--- test.py
+++ test.py
@@ -21,7 +21,6 @@ st.latex('{\displaystyle \sum _{a}W_{ia}h_{a}}\sum _{a}W_{ia}h_{a}')
 uploaded_file = st.file_uploader("Upload Files",type=['csv'])
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file , sep = ';')
-    data.dropna(axis = 'columns', how = 'all', inplace = True)
 
     st.dataframe(data)
 
@@ -31,9 +30,16 @@ if uploaded_file is not None:
 
     st.write(data.equals(newdata))
 
-    st.table(data.var())
-
     X = data.iloc[:,1:]
+
+    var_table = X.var()
+    st.header("Variability")
+    st.write(var_table)
+    st.header('Items with no variability')
+    no_var_list = var_table[var_table == 0].index.tolist()
+    st.write(no_var_list)
+
+    X.drop(columns = no_var_list, inplace = True)
 
     component_err = {}
 
@@ -60,4 +66,36 @@ if uploaded_file is not None:
     df_H = pd.DataFrame(data = H, columns = hm_cols, index = ['comp_' + str(i) for i in range(H.shape[0])])
     ax = sns.heatmap(df_H)
     plt.yticks(rotation=0)
+    st.pyplot(fig)
+
+    #st.bar_chart(df_H)
+
+    st.header('Weights')
+    st.write(W)
+    fig, ax = plt.subplots()
+    df_W = pd.DataFrame(W, columns = ['comp_' + str(i) for i in range(W.shape[1])])
+    ax = sns.heatmap(df_W)
+    st.pyplot(fig)
+
+    from sklearn.cluster import KMeans
+
+    st.header('Weight on component')
+
+    st.write(W)
+
+    number_clusters = 3
+
+    number_clusters = st.number_input('Number of clusters', step = 1)
+
+    kmeans = KMeans(n_clusters=number_clusters, random_state=0).fit(W)
+
+    st.header('KMeans clustering')
+    st.write(kmeans.labels_)
+
+    df_W['group'] = kmeans.labels_
+
+    st.header('Cluster distribution')
+
+    fig = sns.pairplot(df_W, hue = 'group')
+
     st.pyplot(fig)
